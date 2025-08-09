39dbe939727b225c9f8732b13616d6732b0c94cc
Chris Colgan <colganc@hotmail.com>
2025-08-09 06:26:20 -0700

    checkpoint

---
+++ tests/tests.cs
@@ -0,0 +1,10 @@
+﻿namespace tests;
+
+// using osi.Program;
+
+public class extendPistons_should {
+    [Fact]
+    public void extendPistons_should_run() {
+        var osi = new Program();
+    }
+}
